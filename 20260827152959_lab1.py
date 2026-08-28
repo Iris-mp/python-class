96ef0d2ea1b76e259ea33e2e665dfab786be3dd5
Jerry <jerryfu_mc@outlook.com>
2026-08-28 00:08:44 -0400

    reformat code

--- 20260827152959_lab1.py
+++ 20260827152959_lab1.py
@@ -3,6 +3,7 @@
 # Task 1
 # Write a program that prints:
 # Hello, World!
+
 print("Hello, World!")
 
 # Task 2
@@ -10,14 +11,16 @@ print("Hello, World!")
 # - their name (string)
 # - their age (integer)
 # - their height in meters (float)
-name=input("Enter your name:")
-age=int(input("Enter your age:"))
-height=float(input("Enter your height (meters):"))
-print("Hello, "+name+"!")
-print("You are "+str(age)+" years old.")
-print("Your height is "+str(height)+" meters.")
-#
+
+name = input("Enter your name:")
+age = int(input("Enter your age:"))
+height = float(input("Enter your height (meters):"))
+
 # Then display:
 # Hello, <name>!
 # You are <age> years old.
 # Your height is <height> meters.
+
+print("Hello, " + name + "!")
+print("You are " + str(age) + " years old.")
+print("Your height is " + str(height) + " meters.")
