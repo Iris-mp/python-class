7b95970227630595cd07a4d4e86c4a36e7139c5d
Jerry <jerryfu_mc@outlook.com>
2026-08-29 10:24:05 -0400

    fix typo in comment

--- jerry/arithmetic.py
+++ jerry/arithmetic.py
@@ -10,7 +10,7 @@ n *= 2
 print(n)  # 24
 
 # python executes the right hand side (RHS) first, so n + 1 => 24 + 1 = 25,
-# and then runs the addition assignment operator a += RHS => 24 + 25 = 49
+# and then runs the addition assignment operator n += RHS => 24 + 25 = 49
 n += n + 1
 
 print(n)  # 49
